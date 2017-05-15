13904dbd300bf6f804a83c6c0197d5baea45214a
ismaa10 <ismaa10@gmail.com>
2017-05-15 18:20:01 -0300

    Se cambia el remote-path en get

--- fabfile.py
+++ fabfile.py
@@ -247,7 +247,7 @@ def respaldar_osv():
 			#resultado = run('export8k -local')
 			#raw_log.write(resultado)
 		with settings(user=usuario_sbc, password=password_sbc, warn_only=True, command_timeout=300):
-			resultado2 = get('/software/toolkit', carpeta_de_backups + subcarpeta_de_osvs)
+			resultado2 = get('/software/toolkit/*', carpeta_de_backups + subcarpeta_de_osvs)
 		#if resultado.failed or not resultado2.succeeded:			
 		if not resultado2.succeeded:			
 			err_msg = 'ATENCION!!!! ' + traductor[env.host_string][0] + ' Failed!\n'
