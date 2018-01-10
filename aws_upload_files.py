526dc7f352ddbb67203a65a15ccb6776435e1b1c
Marcelo Iriarte <marceiriarte@gmail.com>
2018-01-10 17:54:45 -0300

    Se modifica script de upload a S3 para que suba tambien los reportes cdr del accounting

--- aws_upload_files.py
+++ aws_upload_files.py
@@ -1,10 +1,11 @@
 from aws_utilities import upload_dir_content
 from utilities import send_mail
 
-path_configs = "/configs/"
 path_cloud = "backups"
 
+#Upload configurations backups to AWS S3
 try:
+    path_configs = "/configs/"
     folder = "/FWs/"
     upload_dir_content(path_configs + folder, path_cloud + folder, True)
     folder = "/OSVs/"
@@ -13,6 +14,16 @@ try:
     upload_dir_content(path_configs + folder, path_cloud + folder, False)
     #folder = "/Unify/"
     #upload_dir_content(path_configs + folder, path_cloud + folder, False)
+except Exception as e:
+    print("ERROR! uploading folder: " + folder + "\n")
+    print("Details of error: ", e)
+    send_mail("ERROR uploading folder to S3", str(e))
+
+#Uplado cdr backups to AWS S2
+try:
+    path_configs = "/cdr-backups/"
+    folder = "/CDR-Backups/"
+    upload_dir_content(path_configs, path_cloud + folder, True)
 except Exception as e:
     print("ERROR! uploading folder: " + folder + "\n")
     print("Details of error: ", e)
