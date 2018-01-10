f38eb355085e09caed2da5d945e7652789ebe543
Marcelo Iriarte <marceiriarte@gmail.com>
2018-01-10 17:29:27 -0300

    Se cambia la forma de montar las carpetas del accounting, pasando a la forma de creacion de volumens.

--- cdr_backup.py
+++ cdr_backup.py
@@ -5,35 +5,16 @@ mnt_dest_cdr_osv2 = '/mnt-folder-osv2'
 remote_cdr_osv1 = '/osv1'
 remote_cdr_osv2 = '/osv2'
 
-
+sub_folder_backup_osv1 = '/osv1'
+sub_folder_backup_osv2 = '/osv2'
 credentials_file = '/root/.credentials'
 
-accouting_ip = '172.16.0.105'
+backup_cdr_root = '/cdr-backups/'
 
-backup_cdr_root = '/cdr-backups'
+print ('Starting files copy from : //' + mnt_dest_cdr_osv1 + " folder: " + remote_cdr_osv1)
+os.system("cp -v " + mnt_dest_cdr_osv1 + "/* " + backup_cdr_root)
+print ('Finishing files copy from : //' + mnt_dest_cdr_osv1 + " folder: " + remote_cdr_osv1)
 
-
-print('Mounting remote folder //' + accouting_ip + remote_cdr_osv1 + ' in ' + mnt_dest_cdr_osv1 + '')
-os.system("mount -t cifs -o credentials=" + credentials_file + " //" + accouting_ip + remote_cdr_osv1 + " " +
-          mnt_dest_cdr_osv1 + "")
-
-print ('Starting files copy from : //' + accouting_ip + " folder: " + remote_cdr_osv1)
-os.system("cp -v " + mnt_dest_cdr_osv1 + "/* " + backup_cdr_root + "")
-print ('Finishing files copy from : //' + accouting_ip + " folder: " + remote_cdr_osv1)
-
-print('Unmounting remote folder //' + accouting_ip + remote_cdr_osv1 + '')
-os.system("umount " + mnt_dest_cdr_osv1 + "/")
-
-
-print('Mounting remote folder //' + accouting_ip + remote_cdr_osv2 + ' in ' + mnt_dest_cdr_osv2 + '')
-os.system("mount -t cifs -o credentials=" + credentials_file + " //" + accouting_ip + remote_cdr_osv2 + " " +
-          mnt_dest_cdr_osv2 + "")
-
-print ('Starting files copy from  : //' + accouting_ip + " folder: " + remote_cdr_osv2)
-os.system("cp -v " + mnt_dest_cdr_osv2 + "/* " + backup_cdr_root + "")
-print ('Finishing files copy from : //' + accouting_ip + " folder: " + remote_cdr_osv2)
-
-print('Unmounting remote folder //' + accouting_ip + remote_cdr_osv2 + '')
-os.system("umount " + mnt_dest_cdr_osv2 + "/")
-
-input("Done!...")
+print ('Starting files copy from  : //' + mnt_dest_cdr_osv2 + " folder: " + remote_cdr_osv2)
+os.system("cp -v " + mnt_dest_cdr_osv2 + "/* " + backup_cdr_root)
+print ('Finishing files copy from : //' + mnt_dest_cdr_osv2 + " folder: " + remote_cdr_osv2)
