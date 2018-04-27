abdee700ed3beb1f9d9875ec473c77bd75483fa3
miriarte <C76908hh>
2018-04-27 12:07:37 -0300

    Se modifica codigo para que borre los cdr una vez respaldados y copiados en jusan server

--- aws_upload_files.py
+++ aws_upload_files.py
@@ -34,13 +34,13 @@ except Exception as e:
 #    send_mail("ERROR uploading folder to S3", str(e))
 
 
-#Upload cdr files from OSVs servers to AWS S3
+#Upload cdr files from /home/conatel/config-backups/CDRs to AWS S3 y los borra en origen
 try:
     local_path = "/configs/"
     s3_prefix = "/CDRs"
     s3_prefix_osv = "/OSVs/"
     folder = "/CDRs/"
-    upload_dir_content(local_path + folder, path_cloud + s3_prefix + s3_prefix_osv, False)
+    upload_dir_content(local_path + folder, path_cloud + s3_prefix + s3_prefix_osv, True)
 except Exception as e:
     print("ERROR! updating folder: " + folder + "\n")
     print("Details of error: ", e)
--- cdr_backup.py
+++ cdr_backup.py
@@ -1,22 +1,23 @@
-import os
-
-importiert_folder = '/importiert'
-received_archive_folder = '/received_archive'
-
-credentials_file = '/root/.credentials'
-
-backup_cdr_root = '/cdr-backups'
-
-if not os.path.exists(backup_cdr_root + importiert_folder):
-    os.makedirs(backup_cdr_root + importiert_folder)
-
-if not os.path.exists(backup_cdr_root + received_archive_folder):
-    os.makedirs(backup_cdr_root + received_archive_folder)
-
-print ('Starting files copy from : ' + importiert_folder + " folder to " + backup_cdr_root + importiert_folder)
-os.system("cp -v " + importiert_folder + "/*.cdr " + backup_cdr_root + importiert_folder)
-print ('Finishing copy files from : ' + importiert_folder + " folder to " + backup_cdr_root + importiert_folder)
-
-print ('Starting files copy from : ' + received_archive_folder + " folder to " + backup_cdr_root + received_archive_folder)
-os.system("cp -v " + received_archive_folder + "/*.cdr " + backup_cdr_root + received_archive_folder)
-print ('Finishing copy files from : ' + received_archive_folder + " folder to " + backup_cdr_root + received_archive_folder)
+#TODO: BORRAR, YA QUE NO SE RESPALDAN MAS LOS CDR DESDE EL ACCOUNTING DE UNIFY
+# import os
+#
+# importiert_folder = '/importiert'
+# received_archive_folder = '/received_archive'
+#
+# credentials_file = '/root/.credentials'
+#
+# backup_cdr_root = '/cdr-backups'
+#
+# if not os.path.exists(backup_cdr_root + importiert_folder):
+#     os.makedirs(backup_cdr_root + importiert_folder)
+#
+# if not os.path.exists(backup_cdr_root + received_archive_folder):
+#     os.makedirs(backup_cdr_root + received_archive_folder)
+#
+# print ('Starting files copy from : ' + importiert_folder + " folder to " + backup_cdr_root + importiert_folder)
+# os.system("cp -v " + importiert_folder + "/*.cdr " + backup_cdr_root + importiert_folder)
+# print ('Finishing copy files from : ' + importiert_folder + " folder to " + backup_cdr_root + importiert_folder)
+#
+# print ('Starting files copy from : ' + received_archive_folder + " folder to " + backup_cdr_root + received_archive_folder)
+# os.system("cp -v " + received_archive_folder + "/*.cdr " + backup_cdr_root + received_archive_folder)
+# print ('Finishing copy files from : ' + received_archive_folder + " folder to " + backup_cdr_root + received_archive_folder)
