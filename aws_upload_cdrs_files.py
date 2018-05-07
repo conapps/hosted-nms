b7bc95756d5e0065b56b8837437a19bb03f68ca3
marce <marceiriarte@gmail.com>
2018-05-07 12:41:33 -0300

    Upload only cdr to s3

---
+++ aws_upload_cdrs_files.py
@@ -0,0 +1,15 @@
+from aws_utilities import upload_dir_content
+from utilities import send_mail
+
+#Upload cdr files from /home/conatel/config-backups/CDRs to AWS S3 y los borra en origen
+path_cloud = "backups"
+try:
+    local_path = "/configs/"
+    s3_prefix = "/CDRs"
+    s3_prefix_osv = "/OSVs/"
+    folder = "/CDRs/"
+    upload_dir_content(local_path + folder, path_cloud + s3_prefix + s3_prefix_osv, False)
+except Exception as e:
+    print("ERROR! updating folder: " + folder + "\n")
+    print("Details of error: ", e)
+    send_mail("ERROR uploading folder to S3", str(e))
--- aws_upload_files.py
+++ aws_upload_files.py
@@ -35,16 +35,16 @@ except Exception as e:
 
 
 #Upload cdr files from /home/conatel/config-backups/CDRs to AWS S3 y los borra en origen
-try:
-    local_path = "/configs/"
-    s3_prefix = "/CDRs"
-    s3_prefix_osv = "/OSVs/"
-    folder = "/CDRs/"
-    upload_dir_content(local_path + folder, path_cloud + s3_prefix + s3_prefix_osv, True)
-except Exception as e:
-    print("ERROR! updating folder: " + folder + "\n")
-    print("Details of error: ", e)
-    send_mail("ERROR uploading folder to S3", str(e))
+#try:
+#    local_path = "/configs/"
+#    s3_prefix = "/CDRs"
+#    s3_prefix_osv = "/OSVs/"
+#    folder = "/CDRs/"
+#    upload_dir_content(local_path + folder, path_cloud + s3_prefix + s3_prefix_osv, True)
+#except Exception as e:
+#    print("ERROR! updating folder: " + folder + "\n")
+#    print("Details of error: ", e)
+#    send_mail("ERROR uploading folder to S3", str(e))
 
 
 
